7f82e2dc2965646d594e0d9cc3eb0a2e8335f562
Muhammad Sulthon Imam Puro <75915809+mslthn@users.noreply.github.com>
2025-12-30 16:13:15 +0700

    Add expect function with toBe and notToBe methods
    Implement expect function for testing equality

---
+++ task1.js
@@ -0,0 +1,31 @@
+// 2704. To Be or Not to Be
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe : function(x){
+            
+            if (val === x) {
+                return true
+            } else if (val !== x){
+                return (`"error" : "not equal"`)
+            }
+        },
+        notToBe : function(x){
+            
+            if (val !== x) {
+                return true
+            } else {
+                console.error("Not Equal")
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
