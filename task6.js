b11b8fba6d809a2376d12e002d32371ab5b9fdad
Muhammad Sulthon Imam Puro <75915809+mslthn@users.noreply.github.com>
2025-12-31 00:34:26 +0700

    Add files via upload

---
+++ task6.js
@@ -0,0 +1,16 @@
+// 2718. Create Hello World Function
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
